0d11a5acbe1030cc7f08b1436384169680dbad19
Moon <kwarnkham.moon@gmail.com>
2025-04-04 18:43:26 +0700

    Add high usage alert to alertBot for CPU, RAM, and Disk metrics

--- server.js
+++ server.js
@@ -17,12 +17,16 @@ let cpuExceedTime = 0;
 let ramExceedTime = 0;
 let diskExceedTime = 0;
 
+let counter = 0;
+
 alertBot(`Node program on ${process.env.MY_IP} has been (re)started`)
 
 io.on('connection', (socket) => {
     console.log('Client connected');
 
     const interval = setInterval(() => {
+        counter += 1;
+
         Promise.all([getCPUUsage(), getRAMUsage(), getDiskUsage()])
             .then(([cpuUsage, ramUsage, diskUsage]) => {
                 socket.emit('usage', {
@@ -33,11 +37,25 @@ io.on('connection', (socket) => {
                     memoryThreshold: `${memoryThreshold}%`,
                     diskThreshold: `${diskThreshold}%`
                 });
+                //every 5 minutes
+                if (counter >= 60 * 5) {
+                    counter = 0;
+                    if (cpuUsage >= 70 || ramUsage >= 70 || diskUsage >= 70) {
+                        alertBot(`
+                            Server Name: ${process.env.NAME}
+                            \nIP: ${process.env.MY_IP}
+                            \nStatus: Server is in high usage
+                            \nCPU Usage: ${cpuUsage}%
+                            \nRAM Usage: ${ramUsage}%
+                            \nDisk Usage: ${diskUsage}%
+                            `);
+                    }
+                }
 
                 if (cpuUsage > cpuThreshold) {
                     cpuExceedTime += 1;
                     if (cpuExceedTime >= 10) {
-                        alertBot(`${process.env.MY_IP}: CPU usage exceeded ${cpuThreshold}% for 10 seconds: ${cpuUsage}%`)
+                        // alertBot(`${process.env.MY_IP}: CPU usage exceeded ${cpuThreshold}% for 10 seconds: ${cpuUsage}%`)
                         cpuExceedTime = 0;
                     }
                 } else {
@@ -47,7 +65,7 @@ io.on('connection', (socket) => {
                 if (ramUsage > memoryThreshold) {
                     ramExceedTime += 1;
                     if (ramExceedTime >= 10) {
-                        alertBot(`${process.env.MY_IP}: RAM usage exceeded ${memoryThreshold}% for 10 seconds: ${ramUsage}%`);
+                        // alertBot(`${process.env.MY_IP}: RAM usage exceeded ${memoryThreshold}% for 10 seconds: ${ramUsage}%`);
                         ramExceedTime = 0;
                     }
                 } else {
@@ -57,7 +75,7 @@ io.on('connection', (socket) => {
                 if (diskUsage > diskThreshold) {
                     diskExceedTime += 1;
                     if (diskExceedTime >= 10) {
-                        alertBot(`${process.env.MY_IP}: Disk usage exceeded ${diskThreshold}% for 10 seconds: ${diskUsage}%`);
+                        // alertBot(`${process.env.MY_IP}: Disk usage exceeded ${diskThreshold}% for 10 seconds: ${diskUsage}%`);
                         diskExceedTime = 0;
                     }
                 } else {
